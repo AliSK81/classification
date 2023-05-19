9e10f75a83dbd1d985977658797b23e4b2ef0fdb
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2023-05-19 14:57:38 +0430

    move resnet

--- src/main.py
+++ src/main.py
@@ -9,7 +9,6 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.models import resnet34
 
-# Convert data to a normalized torch.FloatTensor
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
@@ -18,24 +17,13 @@ transform = transforms.Compose([
 train_data = datasets.CIFAR10('data', train=True, download=True, transform=transform)
 test_data = datasets.CIFAR10('data', train=False, download=True, transform=transform)
 
-train_loader = DataLoader(train_data, batch_size=256, shuffle=True)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=100, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=100, shuffle=False)
 
 # Define the feature extractor
 feature_extractor = resnet34(pretrained=True)
 num_features = feature_extractor.fc.in_features
 feature_extractor.fc = nn.Identity()
-
-feature_extractor.eval()
-with torch.no_grad():
-    features_list = []
-    labels_list = []
-    for images, labels in train_loader:
-        features = feature_extractor(images)
-        features_list.append(features)
-        labels_list.append(labels)
-
-x_train = torch.cat(features_list, dim=0).numpy()
-y_train = torch.cat(labels_list, dim=0).numpy()
 
 
 class Dense:
@@ -102,35 +90,45 @@ Loss = CategoricalCrossEntropyLoss()
 Optimizer = SGD(learning_rate=0.001)
 
 num_classes = 10
-y_1hot = np.eye(num_classes)[y_train]
 
 # Main Loop of Training
 for epoch in range(20):
-    # forward
-    output1 = Layer1.forward(x_train)
-    output2 = Act1.forward(output1)
-    output3 = Layer2.forward(output2)
-    y_pred = Act2.forward(output3)
-    loss = Loss.forward(y_pred, y_1hot)
 
-    # Report
-    y_predict = np.argmax(y_pred, axis=1)
-    accuracy = np.mean(y_train == y_predict)
-    print(f'Epoch:{epoch}')
-    print(f'Loss: {loss}')
-    print(f'Accuracy: {accuracy}')
-    print('--------------------------')
+    feature_extractor.eval()
+    with torch.no_grad():
+        for images, labels in train_loader:
+            features = feature_extractor(images)
 
-    # backward
-    d_loss = Loss.backward(y_pred, y_1hot)
-    d_output3 = Act2.backward(d_loss, y_1hot)
-    d_output2, d_biases2, d_output1 = Layer2.backward(d_output3)
-    d_output1 = Act1.backward(d_output1)
-    d_output0, d_biases1, _ = Layer1.backward(d_output1)
+            x_train = features.numpy()
+            y_train = labels.numpy()
 
-    # update params
-    Optimizer.update(Layer1, d_output0, d_biases1)
-    Optimizer.update(Layer2, d_output2, d_biases2)
+            y_1hot = np.eye(num_classes)[y_train]
+
+            # forward
+            output1 = Layer1.forward(x_train)
+            output2 = Act1.forward(output1)
+            output3 = Layer2.forward(output2)
+            y_pred = Act2.forward(output3)
+            loss = Loss.forward(y_pred, y_1hot)
+
+            # Report
+            y_predict = np.argmax(y_pred, axis=1)
+            accuracy = np.mean(y_train == y_predict)
+            print(f'Epoch:{epoch}')
+            print(f'Loss: {loss}')
+            print(f'Accuracy: {accuracy}')
+            print('--------------------------')
+
+            # backward
+            d_loss = Loss.backward(y_pred, y_1hot)
+            d_output3 = Act2.backward(d_loss, y_1hot)
+            d_output2, d_biases2, d_output1 = Layer2.backward(d_output3)
+            d_output1 = Act1.backward(d_output1)
+            d_output0, d_biases1, _ = Layer1.backward(d_output1)
+
+            # update params
+            Optimizer.update(Layer1, d_output0, d_biases1)
+            Optimizer.update(Layer2, d_output2, d_biases2)
 
 # Confusion Matrix for the training set
 cm_train = confusion_matrix(y_train, y_predict)
