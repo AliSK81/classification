78785838f2d3e3a7118a14468d8b9d5da4f7be4c
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2023-05-19 17:53:49 +0430

    temp

--- src/main.py
+++ src/main.py
@@ -28,70 +28,61 @@ feature_extractor.fc = nn.Identity()
 
 class Dense:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = np.random.randn(n_inputs, n_neurons) * 0.01
+        self.weights = np.random.randn(n_inputs, n_neurons) * np.sqrt(2 / n_inputs)
         self.biases = np.zeros((1, n_neurons))
 
     def forward(self, inputs):
         self.inputs = inputs
-        return np.dot(inputs, self.weights) + self.biases
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-    def backward(self, d_outputs):
-        d_weights = np.dot(self.inputs.T, d_outputs)
-        d_biases = np.sum(d_outputs, axis=0, keepdims=True)
-        d_inputs = np.dot(d_outputs, self.weights.T)
-
-        return d_weights, d_biases, d_inputs
+    def backward(self, b_input):
+        self.d_weights = np.dot(self.inputs.T, b_input)
+        self.d_biases = np.sum(b_input, axis=0, keepdims=True)
+        self.b_output = np.dot(b_input, self.weights.T)
 
 
 class ReLU:
     def forward(self, inputs):
         self.inputs = inputs
-        return np.maximum(0, inputs)
+        self.output = np.maximum(0, inputs)
 
-    def backward(self, d_outputs):
-        return d_outputs * (self.inputs > 0).astype(int)
+    def backward(self, b_input):
+        self.b_output = b_input.copy()
+        self.b_output[self.inputs <= 0] = 0
 
 
 class Softmax:
     def forward(self, inputs):
-        exp_vals = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        self.output = exp_vals / np.sum(exp_vals, axis=1, keepdims=True)
-        return self.output
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
 
-    def backward(self, b_input, y_true):
-        batch_size = b_input.shape[0]
-        jacobian_matrix = np.zeros((batch_size, b_input.shape[1], b_input.shape[1]))
-        for i in range(batch_size):
-            for j in range(b_input.shape[1]):
-                for k in range(b_input.shape[1]):
-                    if j == k:
-                        jacobian_matrix[i][j][k] = self.output[i][j] * (1 - self.output[i][k])
-                    else:
-                        jacobian_matrix[i][j][k] = -self.output[i][j] * self.output[i][k]
-
-        self.b_output = np.matmul(b_input[:, np.newaxis, :], jacobian_matrix).squeeze()
-        return self.b_output
+    def backward(self, b_input):
+        self.b_output = b_input.copy()
 
 
 class CategoricalCrossEntropyLoss:
-    def forward(self, y_pred, y_true):
-        batch_size = y_pred.shape[0]
-        return -np.sum(y_true * np.log(y_pred + 1e-9)) / batch_size
+    def forward(self, softmax_output, class_label):
+        n_samples = len(softmax_output)
+        correct_class_probabilities = softmax_output[range(n_samples), class_label.argmax(axis=1)]
+        loss = -np.mean(np.log(correct_class_probabilities + 1e-7))
+        self.output = loss
+        return loss
 
-    def backward(self, y_pred, y_true):
-        # batch_size = y_pred.shape[0]
-        # return (y_pred - y_true) / batch_size
-        self.b_output = - y_true / y_pred
-        return self.b_output
+    def backward(self, softmax_output, class_label):
+        n_samples = len(softmax_output)
+        self.b_output = softmax_output.copy()
+        self.b_output[range(n_samples), class_label.argmax(axis=1)] -= 1
+        self.b_output /= n_samples
 
 
 class SGD:
     def __init__(self, learning_rate=0.001):
         self.learning_rate = learning_rate
 
-    def update(self, layer, d_weights, d_biases):
-        layer.weights -= self.learning_rate * d_weights
-        layer.biases -= self.learning_rate * d_biases
+    def update(self, layer):
+        layer.weights -= self.learning_rate * layer.d_weights
+        layer.biases -= self.learning_rate * layer.d_biases
 
 
 d = num_features
@@ -118,14 +109,14 @@ for epoch in range(20):
             y_1hot = np.eye(num_classes)[y_train]
 
             # forward
-            output1 = Layer1.forward(x_train)
-            output2 = Act1.forward(output1)
-            output3 = Layer2.forward(output2)
-            y_pred = Act2.forward(output3)
-            loss = Loss.forward(y_pred, y_1hot)
+            Layer1.forward(x_train)
+            Act1.forward(Layer1.output)
+            Layer2.forward(Act1.output)
+            Act2.forward(Layer2.output)
+            loss = Loss.forward(Act2.output, y_1hot)
 
             # Report
-            y_predict = np.argmax(y_pred, axis=1)
+            y_predict = np.argmax(Act2.output, axis=1)
             accuracy = np.mean(y_train == y_predict)
             print(f'Epoch:{epoch}')
             print(f'Loss: {loss}')
@@ -133,15 +124,15 @@ for epoch in range(20):
             print('--------------------------')
 
             # backward
-            d_loss = Loss.backward(y_pred, y_1hot)
-            d_output3 = Act2.backward(d_loss, y_1hot)
-            d_output2, d_biases2, d_output1 = Layer2.backward(d_output3)
-            d_output1 = Act1.backward(d_output1)
-            d_output0, d_biases1, _ = Layer1.backward(d_output1)
+            Loss.backward(Act2.output, y_1hot)
+            Act2.backward(Loss.b_output)
+            Layer2.backward(Act2.b_output)
+            Act1.backward(Layer2.b_output)
+            Layer1.backward(Act1.b_output)
 
             # update params
-            Optimizer.update(Layer1, d_output0, d_biases1)
-            Optimizer.update(Layer2, d_output2, d_biases2)
+            Optimizer.update(Layer1)
+            Optimizer.update(Layer2)
 
 # Confusion Matrix for the training set
 cm_train = confusion_matrix(y_train, y_predict)
