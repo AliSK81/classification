5bef44a92c99ddc67021e17371cf7152713740b3
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2023-05-19 11:47:48 +0430

    fix dimension bugs

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-# torch is just for the feature extractor and the dataset (NOT FOR IMPLEMENTING NEURAL NETWORKS!)
 import numpy as np
 import seaborn as sb
 import torch
@@ -10,21 +9,16 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.models import resnet34
 
-# sklearn is just for evaluation (NOT FOR IMPLEMENTING NEURAL NETWORKS!)
-
-# convert data to a normalized torch.FloatTensor
+# Convert data to a normalized torch.FloatTensor
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
-train_data = datasets.CIFAR10('data', train=True, download=False, transform=transform)
-test_data = datasets.CIFAR10('data', train=False, download=False, transform=transform)
+train_data = datasets.CIFAR10('data', train=True, download=True, transform=transform)
+test_data = datasets.CIFAR10('data', train=False, download=True, transform=transform)
 
 train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
-
-# You should define x_train and y_train
-
 
 # Define the feature extractor
 feature_extractor = resnet34(pretrained=True)
@@ -34,52 +28,41 @@ feature_extractor.fc = nn.Identity()
 feature_extractor.eval()
 with torch.no_grad():
     features_list = []
-    y_train = []
-    i = 0
+    labels_list = []
     for images, labels in train_loader:
-        i += 1
-        if i == 2:
-            break
         features = feature_extractor(images)
         features_list.append(features)
-        y_train.append(labels)
+        labels_list.append(labels)
+        break
 
-    features_train = torch.cat(features_list, dim=0)
-
-x_train = features_train.numpy()
-y_train = torch.cat(y_train, dim=0).numpy()
+x_train = torch.cat(features_list, dim=0).numpy()
+y_train = torch.cat(labels_list, dim=0).numpy()
 
 
 class Dense:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = np.random.randn(n_inputs, n_neurons)
+        self.weights = np.random.randn(n_inputs, n_neurons) * 0.01
         self.biases = np.zeros((1, n_neurons))
-        self.inputs = None
-        self.f_output = None
-        self.b_output = None
 
     def forward(self, inputs):
         self.inputs = inputs
-        self.f_output = np.dot(inputs, self.weights) + self.biases
+        return np.dot(inputs, self.weights) + self.biases
 
     def backward(self, b_input):
-        self.weights = np.dot(self.inputs.T, b_input)
-        self.biases = np.sum(b_input, axis=0, keepdims=True)
-        self.b_output = np.dot(b_input, self.weights.T)
+        d_weights = np.dot(self.inputs.T, b_input)
+        d_biases = np.sum(b_input, axis=0, keepdims=True)
+        d_inputs = np.dot(b_input, self.weights.T)
+
+        return d_weights, d_biases, d_inputs
 
 
 class ReLU:
-    def __init__(self):
-        self.b_output = None
-        self.inputs = None
-        self.f_output = None
-
     def forward(self, inputs):
         self.inputs = inputs
-        self.f_output = np.maximum(0, inputs)
+        return np.maximum(0, inputs)
 
     def backward(self, b_input):
-        self.b_output = b_input * (self.inputs > 0)
+        return b_input * (self.inputs > 0)
 
 
 class Sigmoid:
@@ -90,88 +73,73 @@ class Sigmoid:
     def forward(self, inputs):
         self.output = 1 / (1 + np.exp(-inputs))
 
-    def backward(self, b_input):
-        self.b_output = b_input * self.output * (1 - self.output)
-
 
 class Softmax:
-    def __init__(self):
-        self.output = None
-        self.b_output = None
-
     def forward(self, inputs):
-        exp_vals = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        self.output = exp_vals / np.sum(exp_vals, axis=1, keepdims=True)
+        # exp_vals = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        exp_vals = inputs
+        return exp_vals / np.sum(exp_vals, axis=1, keepdims=True)
 
-    def backward(self, b_input):
-        self.b_output = b_input
+    def backward(self, b_input, y_true):
+        return b_input - y_true
 
 
-class Categorical_Cross_Entropy_loss:
-    def __init__(self):
-        self.value = None
-        self.b_output = None
+class CategoricalCrossEntropyLoss:
+    def forward(self, y_pred, y_true):
+        batch_size = y_pred.shape[0]
+        return -np.sum(y_true * np.log(y_pred)) / batch_size
 
-    def forward(self, softmax_output, class_label):
-        softmax_output = softmax_output
-        num_samples = softmax_output.shape[1]
-        self.value = -np.sum(class_label * np.log(softmax_output)) / num_samples
-
-    def backward(self, softmax_output, class_label):
-        num_samples = softmax_output.shape[0]
-        grad_softmax = softmax_output - class_label
-        grad_softmax /= num_samples
-        self.b_output = grad_softmax
+    def backward(self, y_pred, y_true):
+        return y_pred - y_true
 
 
 class SGD:
     def __init__(self, learning_rate=0.001):
         self.learning_rate = learning_rate
 
-    def update(self, layer):
-        layer.weights -= self.learning_rate * layer.weights
-        layer.biases -= self.learning_rate * layer.biases
+    def update(self, layer, d_weights, d_biases):
+        layer.weights -= self.learning_rate * d_weights
+        layer.biases -= self.learning_rate * d_biases
 
 
-feature_extractor = resnet34(pretrained=True)
-d = feature_extractor.fc.in_features
-
-Layer1 = Dense(d, 20)  # d is the output dimension of feature extractor
+d = num_features
+Layer1 = Dense(d, 20)
 Act1 = ReLU()
 Layer2 = Dense(20, 10)
 Act2 = Softmax()
-Loss = Categorical_Cross_Entropy_loss()
+Loss = CategoricalCrossEntropyLoss()
 Optimizer = SGD(learning_rate=0.001)
+
 num_classes = 10
 y_1hot = np.eye(num_classes)[y_train]
 
 # Main Loop of Training
 for epoch in range(20):
     # forward
-    Layer1.forward(x_train)
-    Act1.forward(Layer1.f_output)
-    Layer2.forward(Act1.f_output)
-    Act2.forward(Layer2.f_output)
-    Loss.forward(Act2.output, y_1hot)
+    output1 = Layer1.forward(x_train)
+    output2 = Act1.forward(output1)
+    output3 = Layer2.forward(output2)
+    y_pred = Act2.forward(output3)
+    loss = Loss.forward(y_pred, y_1hot)
 
     # Report
-    y_predict = np.argmax(Act2.output, axis=1)
+    y_predict = np.argmax(y_pred, axis=1)
     accuracy = np.mean(y_train == y_predict)
     print(f'Epoch:{epoch}')
-    print(f'Loss: {Loss.value}')
+    print(f'Loss: {loss}')
     print(f'Accuracy: {accuracy}')
     print('--------------------------')
 
     # backward
-    Loss.backward(Act2.output, y_1hot)
-    Act2.backward(Loss.b_output)
-    Layer2.backward(Act2.b_output)
-    Act1.backward(Layer2.b_output)
-    Layer1.backward(Act1.b_output)
+    d_loss = Loss.backward(y_pred, y_1hot)
+    d_output3 = Act2.backward(d_loss, y_1hot)
+    d_output2, d_biases2, d_output1 = Layer2.backward(d_output3)
+    d_output1 = Act1.backward(d_output1)
+    d_output0, d_biases1, _ = Layer1.backward(d_output1)
 
     # update params
-    Optimizer.update(Layer1)
-    Optimizer.update(Layer2)
+    Optimizer.update(Layer1, d_output0, d_biases1)
+    Optimizer.update(Layer2, d_output2, d_biases2)
 
 # Confusion Matrix for the training set
 cm_train = confusion_matrix(y_train, y_predict)
